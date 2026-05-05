e7e746f7cdd15dc055b3a6c5027d9c825ffc015c
GOSTH <augustusdm8@gmail.com>
2026-05-05 17:07:15 -0500

    feat: add main queue + deadletter

---
+++ sqs.tf
@@ -0,0 +1,22 @@
+resource "aws_sqs_queue" "main_queue" {
+  name = "image-processor-${terraform.workspace}-image-queue"
+  max_message_size          = 2048
+  delay_seconds             = 90
+  visibility_timeout_seconds = 360
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 20
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.deadletter_queue.arn
+    maxReceiveCount     = 3 
+  })
+}
+
+resource "aws_sqs_queue" "deadletter_queue" {
+  name                      = "image-processor-${terraform.workspace}-image-dlq"
+  message_retention_seconds = 1209600
+  max_message_size          = 2048
+  delay_seconds             = 90
+  visibility_timeout_seconds = 360
+  receive_wait_time_seconds  = 20
+}
+

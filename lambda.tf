0a6780e5331ce457cf441c131231e76c5d1b6e64
GOSTH <augustusdm8@gmail.com>
2026-05-05 18:01:06 -0500

    feat: agregacion de iam necesario para lambda y configuracion vpc

--- lambda.tf
+++ lambda.tf
@@ -17,4 +17,23 @@ resource "aws_lambda_function" "upload_lambda" {
     }
   }
 
+  vpc_config {
+    subnet_ids         = [
+      aws_subnet.private_subnet_a.id, 
+      aws_subnet.private_subnet_b.id  
+    ]
+    security_group_ids = [aws_security_group.sg_upload_lambda.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.vpc_access_policy,
+    aws_cloudwatch_log_group.lambda_logs
+  ]
+}
+
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "src/lambda" 
+  output_path = "src/function.zip"
 }

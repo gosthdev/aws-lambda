b7c3f676905994a65e8d134428a259d7e60b7eb4
GOSTH <augustusdm8@gmail.com>
2026-05-05 19:11:04 -0500

    feat: permisos necesarios para que lambda pueda comunicarse con s3

--- iam.tf
+++ iam.tf
@@ -23,3 +23,21 @@ resource "aws_iam_role_policy_attachment" "vpc_access_policy" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
+
+data "aws_iam_policy_document" "s3_upload_policy_doc" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.s3_bucket.arn}/uploads/*"]
+  }
+}
+
+resource "aws_iam_policy" "s3_upload_policy" {
+  name   = "UploadLambdaS3Policy"
+  policy = data.aws_iam_policy_document.s3_upload_policy_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "s3_upload_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.s3_upload_policy.arn
+}

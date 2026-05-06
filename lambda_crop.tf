9116f061889870392a8e12566430f765a7f4d56d
GOSTH <augustusdm8@gmail.com>
2026-05-05 23:45:54 -0500

    feat: añadir lambda a vpc, reglas iam y cloudwatch

--- lambda_crop.tf
+++ lambda_crop.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "crop_lambda" {
-  filename      = data.archive_file.lambda_zip.output_path
-  function_name = "crop_lambda"
+  filename      = data.archive_file.crop_lambda_zip.output_path
+  function_name = "crop-lambda"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.handler"
-  code_sha256   = data.archive_file.lambda_zip.output_base64sha256
+  code_sha256   = data.archive_file.crop_lambda_zip.output_base64sha256
 
   runtime = "nodejs20.x"
   memory_size = 256
@@ -15,10 +15,28 @@ resource "aws_lambda_function" "crop_lambda" {
       PROCESSED_PREFIX   = "processed/"
     }
   }
+
+  vpc_config {
+    subnet_ids         = [
+      aws_subnet.private_subnet_a.id,
+      aws_subnet.private_subnet_b.id
+    ]
+    security_group_ids = [aws_security_group.sg_upload_lambda.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.vpc_access_policy,
+    aws_cloudwatch_log_group.xxxxx
+  ]
 }
 
-data "archive_file" "lambda_zip" {
+data "archive_file" "crop_lambda_zip" {
   type        = "zip"
-  source_file = "src/lambda/index.js"
-  output_path = "src/lambda/function.zip"
+  source_dir  = "src/lambda_crop"
+  output_path = "src/crop_function.zip"
+}
+
+resource "aws_cloudwatch_log_group" "crop_lambda_logs" {
+  name              = "/aws/lambda/lambda-crop"
+  retention_in_days = 14
 }
--- lambda_upload.tf
+++ lambda_upload.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "upload_lambda" {
-  filename      = data.archive_file.lambda_zip.output_path
+  filename      = data.archive_file.upload_lambda_zip.output_path
   function_name = "upload-lambda"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.handler"
-  code_sha256   = data.archive_file.lambda_zip.output_base64sha256
+  code_sha256   = data.archive_file.upload_lambda_zip.output_base64sha256
 
   runtime = "nodejs20.x"
   
@@ -31,9 +31,9 @@ resource "aws_lambda_function" "upload_lambda" {
   ]
 }
 
-data "archive_file" "lambda_zip" {
+data "archive_file" "upload_lambda_zip" {
   type        = "zip"
-  source_dir  = "src/lambda" 
+  source_dir  = "src/lambda_upload" 
   output_path = "src/function.zip"
 }
 

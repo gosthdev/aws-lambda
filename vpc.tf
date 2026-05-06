495d49d2a16d635de40f97b2ed6e1d03a0f05d87
GOSTH <augustusdm8@gmail.com>
2026-05-05 20:42:53 -0500

    feat: rutas publicas

--- vpc.tf
+++ vpc.tf
@@ -72,3 +72,27 @@ resource "aws_subnet" "public_subnet_b" {
     Name = "Public Subnet AZ-b"
   }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_rt_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_rt.id
+}

93c9e7b71a54fb9cbd66bf1a16fc6c4d478a0f96
GOSTH <augustusdm8@gmail.com>
2026-05-06 22:15:47 +0000

    fix: actualizar nombres de recursos

--- lambda_crop.tf
+++ lambda_crop.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "crop_lambda" {
   filename      = data.archive_file.crop_lambda_zip.output_path
-  function_name = "crop-lambda"
+  function_name = "crop-lambda.${workspace}"
   role          = aws_iam_role.lambda_crop_role.arn
   handler       = "index.handler"
   code_sha256   = data.archive_file.crop_lambda_zip.output_base64sha256
